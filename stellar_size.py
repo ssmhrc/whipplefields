5cd9115280e0bf41d9d9836b74a524694bae6b0d
John Livingston <jckhnk@yahoo.com>
2014-01-16 23:54:13 -0800

    added functions to cull down the datasets based on source neighborhood and angular size criteria

--- stellar_size.py
+++ stellar_size.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import urllib
-from astropy.io.votable import parse
+from astropy.io.votable import parse, writeto
+from scipy.spatial import cKDTree as KDT
+from itertools import product
 
 def get_url(ra,dec,radius,brightlim,faintlim):
 	"""
@@ -16,7 +18,7 @@ def get_url(ra,dec,radius,brightlim,faintlim):
 	args = [ra, dec, radius, brightlim, faintlim]
 	ra, dec, radius, brightlim, faintlim = [str(i) for i in args]
 	url='http://www.nofs.navy.mil/cgi-bin/vo_cone.cgi?CAT=NOMAD'+\
-	'&RA='+ra+'&DEC='+dec+'&SR='+radius+'&VERB=3&clr=R2'+\
+	'&RA='+ra+'&DEC='+dec+'&SR='+radius+'&VERB=3&clr=V'+\
 	'&bri='+brightlim+'&fai='+faintlim+\
 	'&cftype=XML/VO&slf=ddd.dd/dd.ddd&skey=RA'
 	return url
@@ -74,8 +76,8 @@ def get_ang_size_wang(table):
 	group2 = 2.0 < v-k		# giants
 	theta1 = 10**(0.453 + 0.246 * (v-k)[group1] - 0.2 * v[group1])
 	theta2 = 10**(0.407 + 0.238 * (v-k)[group2] - 0.2 * v[group2])
-	array1 = np.c_[b[group1], k[group1], theta1, np.ones(theta1.size)]
-	array2 = np.c_[b[group2], k[group2], theta2, np.ones(theta2.size)+1]
+	array1 = np.c_[v[group1], k[group1], theta1, np.ones(theta1.size)]
+	array2 = np.c_[v[group2], k[group2], theta2, np.ones(theta2.size)+1]
 	df = pd.DataFrame(np.r_[array1, array2], 
 		columns=['V','K','theta','group'])
 	return df
@@ -96,26 +98,168 @@ def plot_groupt_hist(df, theta_max):
 	plt.xlabel('angular size [arcsec]')
 	plt.show()
 
-def save_table(table, outpath):
-	"""
-	Saves the data in <table> in CSV format to the path specified by 
-	<outpath>.
-	"""
-	# df = pd.DataFrame(table.array.tolist(), 
-	# 	columns=table.array.dtype.names)
-	df = pd.DataFrame.from_records(table.array)
-	df.to_csv(outpath, index=False)
+# # get data for 'murray1' field and calculate angular sizes using the
+# # relations from the Van Belle 1999 paper
+# # votable = get_votable(78.0, 40.0, 1.41, 10.0, 15.0)
+# vot = get_votable(78.0, 40.0, 0.565, 10.0, 15.0)
+# table = votable.get_first_table()
+# theta_vb = get_ang_size_van_belle(table)
+# plot_groupt_hist(theta_vb,0.3)
+# plot_groupt_hist(theta_vb,0.1)
 
-# get data for 'murray1' field and calculate angular sizes using the
-# relations from the Van Belle 1999 paper
-votable = get_table(78.0, 40.0, 1.41, 10.0, 16.0)
-table = votable.get_first_table()
-save_table(table,'murray1_nomad_test.csv')
-theta_vb = get_ang_size_van_belle(table)
-plot_groupt_hist(theta_vb,0.3)
-plot_groupt_hist(theta_vb,0.1)
+# # now calculate angular size using the relations from Wang et al. 2010 paper
+# theta_wang = get_ang_size_wang(table)
+# plot_groupt_hist(theta_wang,0.3)
+# plot_groupt_hist(theta_wang,0.1)
 
-# now calculate angular size using the relations from Wang et al. 2010 paper
-theta_wang = get_ang_size_wang(table)
-plot_groupt_hist(theta_wang,0.3)
-plot_groupt_hist(theta_wang,0.1)
+# now add more functions to handle checking for too-close neighbors
+def spherical_to_cartesian(ra, dec):
+	"""
+	Inputs in degrees.  Outputs x,y,z
+	"""
+	rar = np.radians(ra)
+	decr = np.radians(dec)
+	x = np.cos(rar) * np.cos(decr)
+	y = np.sin(rar) * np.cos(decr)
+	z = np.sin(decr)
+	return x, y, z
+
+def radec_to_coords(ra, dec):
+	"""
+	Helper function for constructing/querying k-d trees with coordinates
+	in spherical geometry.
+	Converts the input arrays from spherical coordinates to cartesion
+	and populates a 3-dimensional array with the result.
+	"""
+	x, y, z = spherical_to_cartesian(ra, dec)
+	coords = np.empty((x.size, 3))
+	coords[:, 0] = x
+	coords[:, 1] = y
+	coords[:, 2] = z
+	return coords
+
+def great_circle_distance(ra1, dec1, ra2, dec2):
+	"""
+	Returns great circle distance.  Inputs in degrees.
+
+	Uses vicenty distance formula - a bit slower than others, but
+	numerically stable.
+	"""	
+
+	# terminology from the Vicenty formula - lambda and phi and
+	# "standpoint" and "forepoint"
+	lambs = np.radians(ra1)
+	phis = np.radians(dec1)
+	lambf = np.radians(ra2)
+	phif = np.radians(dec2)
+
+	dlamb = lambf - lambs
+
+	numera = np.cos(phif) * np.sin(dlamb)
+	numerb = np.cos(phis) * np.sin(phif) - np.sin(phis) * \
+		np.cos(phif) * np.cos(dlamb)
+	numer = np.hypot(numera, numerb)
+	denom = np.sin(phis) * np.sin(phif) + np.cos(phis) * \
+		np.cos(phif) * np.cos(dlamb)
+	return np.degrees(np.arctan2(numer, denom))
+
+def get_ang_size(df):
+	"""
+	Expects a DataFrame object containing V and K band magnitudes
+	and calculates the resulting angular size in arcsec
+	using the relations from the 2010 Wang et al. paper:
+	http://adsabs.harvard.edu/abs/2010AJ....139.2003W
+	"""
+	group1 = df.V - df.K <= 1.85	# main sequence
+	group2 = 2.0 < df.V - df.K		# giants
+	theta1 = 10**(0.453 + 0.246 * (df.V - df.K)[group1] - 0.2 * df.V[group1])
+	theta2 = 10**(0.407 + 0.238 * (df.V - df.K)[group2] - 0.2 * df.V[group2])
+	theta = pd.Series(np.zeros(df.shape[0]))
+	group = pd.Series(np.zeros(df.shape[0]))
+	theta[group1] = theta1
+	theta[group2] = theta2
+	group[group1] = 1
+	group[group2] = 2
+	df['theta'] = theta
+	df['group'] = group
+	# throw away sources with 1.85 < V-K <= 2
+	df = df[group1 | group2]
+	return df
+
+def cull_dataset(table):
+	"""
+	Efficiently finds all neighbors within 0.01 degrees using
+	kdt.query_ball_point method to get points within radius d, where
+	d is the cartesian distance equivalent to 0.01 degree separation
+	resulting from calculation:
+
+	ra1, ra2 = 0, 0.01
+	dec1, dec2 = 0, 0
+	c1 = spherical_to_cartesian(ra1, dec1)
+	c2 = spherical_to_cartesian(ra2, dec2)
+	d = np.sqrt(sum( [ (c1[i] - c2[i])**2 for i in range(3) ] ))
+
+	If there are any neighbors within 0.01 degrees of a given source,
+	and if any of these neighbors are brighter than 2 magnitudes fainter
+	than the source, remove the source from the table. Also use the
+	Wang et al. 2012 relations to get the angular size of each source and
+	remove any sources with angular size greater than 0.01 arcsec.
+
+	Returns a pandas DataFrame object containing the culled dataset.
+	"""
+
+	good = (table.array['V'] != 30.0) & (table.array['K'] != 30.0)
+	arr = table.array[good]
+	df = get_ang_size(pd.DataFrame.from_records(arr))
+
+	# ignore sources with theta >= 0.01 arcsec
+	df = df[df.theta < 0.01]
+
+	ra, dec, Vmag = [np.array(i) for i in [df.RA, df.DEC, df.V]]
+	kdt = KDT(radec_to_coords(ra, dec))
+	d = 0.00017453292497790891
+	neighbor_flag = np.zeros(df.shape[0])
+
+	for i in range(df.shape[0]):
+
+		coords = radec_to_coords(ra[i],dec[i])
+
+		# skip the first returned index - this is the query point itself
+		idx = kdt.query_ball_point(coords,d)[0][1:]
+
+		if len(idx) < 1:
+			continue
+
+		ds = great_circle_distance(ra_i,dec_i,ra[idx],dec[idx])[0]
+		Vmag_i = Vmag[i]
+		Vmag_neighbors = Vmag[idx]
+
+		# flag sources that have bright nearby neighbors as bad
+		for Vmag_j in Vmag_neighbors:
+			if Vmag_j - Vmag_i < 2:
+				neighbor_flag[i] = 1
+
+	df = df[neighbor_flag==0]
+	return df
+
+vot = get_votable(78.0, 40.0, 0.564, 10.0, 15.0)
+df = cull_dataset(vot.get_first_table())
+
+def sample_sky(ra, dec):
+	"""
+	Returns a list of the counts of viable sources per square degree
+	for the input lists of RA and Dec.
+	"""
+	assert len(ra) == len(dec)
+	deg_sq_radius = np.sqrt(1/np.pi)
+	counts = []
+	for i in range(len(ra)):
+		vot = get_votable(ra[i], dec[i], deg_sq_radius, 10.0, 15.0)
+		df = cull_dataset(vot.get_first_table())
+		name = '_'.join([str(j) for j in [ra, dec]])
+		vot_path = name+'_VOTable.xml'
+		writeto(vot,vot_path)
+		csv_path = name+'_culled.csv'
+		df.to_csv(csv_path, index=False)
+		counts.append(df.shape[0])
+	return counts
